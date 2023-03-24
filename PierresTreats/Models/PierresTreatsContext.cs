af9afde250a35ce22e8d945a506e43eeac0225c1
Kai Clausen <kaiclausen123@gmail.com>
2023-03-24 13:20:23 -0700

    add userTreats to context

--- PierresTreats/Models/PierresTreatsContext.cs
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -8,6 +8,7 @@ namespace PierresTreats.Models
     public DbSet<Treat> Treats { get; set; }
     public DbSet<Flavor> Flavors { get; set; }
     public DbSet<TreatFlavor> TreatFlavors { get; set; }
+    public DbSet<UserTreat> UserTreats { get; set; }
     public PierresTreatsContext(DbContextOptions options) : base(options) { }
   }
 }
