7cdf954a57fe5c28da6bbf438c789124818db2be
Kai Clausen <kaiclausen123@gmail.com>
2023-03-24 12:06:30 -0700

    Make TreatName and StockQuantity Fields Required... Breaking for lunch

--- PierresTreats/Models/Treats.cs
+++ PierresTreats/Models/Treats.cs
@@ -1,12 +1,14 @@
-using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PierresTreats.Models
 {
   public class Treat
   {
     public int TreatId { get; set; }
+    [Required(ErrorMessage = "You must enter a treat name!")]
     public string TreatName { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "You must enter a number greater than or equal to 1.")]
     public int StockQuantity { get; set; }
     public List<TreatFlavor> JoinEntities { get; set; }
   }
