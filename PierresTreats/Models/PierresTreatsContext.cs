229b3a16e53d1279bb06adfc88d1abbb2b62a7f4
Kai Clausen <kaiclausen123@gmail.com>
2023-03-24 08:58:01 -0700

    create db context file

---
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace PierresTreats.Models
+{
+  public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<TreatFlavor> TreatFlavors { get; set; }
+    public PierresTreatsContext(DbContextOptions options) : base(options) { }
+  }
+}
