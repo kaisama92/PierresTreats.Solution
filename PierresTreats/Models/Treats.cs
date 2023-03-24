e1f131a22d0ff5cac418cfb0d2b9692408c76a30
Kai Clausen <kaiclausen123@gmail.com>
2023-03-24 08:42:24 -0700

    create Treats Model

---
+++ PierresTreats/Models/Treats.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+namespace PierresTreats.Models
+{
+  public class Treats
+  {
+    public int TreatsId { get; set; }
+    public string TreatsName { get; set; }
+  }
+}
