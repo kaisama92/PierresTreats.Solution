7de74ad8c3efcd1b5f9c0b6226425ba787be153c
Kai Clausen <kaiclausen123@gmail.com>
2023-03-24 14:38:55 -0700

    add userId to User.cs

--- PierresTreats/Models/User.cs
+++ PierresTreats/Models/User.cs
@@ -2,8 +2,9 @@ using System.Collections.Generic;
 
 namespace PierresTreats.Models
 {
-  public class User
+  public class User 
   {
+    public int UserId { get; set; }
     public string UserName { get; set; }
     public List<UserTreat> JoinEntity2 { get; set; }
   }
